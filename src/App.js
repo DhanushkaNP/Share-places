bfa3934ab4d3568782f4be1b79964406306efa45
Benzter <Dhanushkanuwanpremasiri28@gmail.com>
2023-03-16 15:00:26 +0530

    Add Auth component to authenticate users. And also that enable users to signup

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import NewPlace from "./places/pages/NewPlace";
 import UpdatePlace from "./places/pages/UpdatePlace";
 import UserPlaces from "./places/pages/UserPlaces";
 import MainNavigation from "./shared/components/Navigation/MainNavigation";
+import Auth from "./user/pages/Auth";
 import Users from "./user/pages/Users";
 
 function App() {
@@ -27,6 +28,7 @@ function App() {
           <Route path="/:userId/places" element={<UserPlaces />} />
           <Route path="/places/new" element={<NewPlace />} />
           <Route path="/places/:placeId" element={<UpdatePlace />} />
+          <Route path="/auth" element={<Auth />} />
           <Route path="*" element={<Users />} />
         </Routes>
       </main>
---
+++ src/user/pages/Auth.js
@@ -0,0 +1,100 @@
+import { useState } from "react";
+import { useForm } from "../../shared/hooks/form-hook";
+import Input from "../../shared/components/FormElements/Input";
+import Button from "../../shared/components/FormElements/Button";
+import {
+  VALIDATOR_EMAIL,
+  VALIDATOR_MINLENGTH,
+  VALIDATOR_REQUIRE,
+} from "../../shared/components/util/validators";
+import "./Auth.css";
+import Card from "../../shared/components/UIElements/Card";
+
+function Auth() {
+  const [isLoging, setIsLoging] = useState(true);
+  const [formState, inputHandler, setFormData] = useForm(
+    {
+      email: {
+        value: "",
+        isValid: false,
+      },
+      password: {
+        value: "",
+        isValid: false,
+      },
+    },
+    false
+  );
+
+  function loginHandler(event) {
+    event.preventDefault();
+    console.log(formState.inputs);
+  }
+
+  function switchModeHandler() {
+    if (!isLoging) {
+      setFormData(
+        { ...formState.inputs, name: undefined },
+        formState.inputs.email.isValid && formState.inputs.password.isValid
+      );
+    } else {
+      setFormData(
+        {
+          ...formState.inputs,
+          name: {
+            value: "",
+            isValid: false,
+          },
+        },
+        false
+      );
+    }
+    setIsLoging((prevVlaue) => !prevVlaue);
+  }
+
+  return (
+    <Card className="authentication">
+      <h2>{isLoging ? "Login" : "Signup"} Required</h2>
+      <hr />
+      <form onSubmit={loginHandler}>
+        {!isLoging && (
+          <Input
+            element="input"
+            type="text"
+            id="name"
+            label="Name"
+            errorText="Please Enter a Name here"
+            validators={[VALIDATOR_REQUIRE()]}
+            onInput={inputHandler}
+          />
+        )}
+        <Input
+          element="input"
+          type="email"
+          id="email"
+          label="Email"
+          errorText="Please Enter a valid Email address"
+          validators={[VALIDATOR_EMAIL()]}
+          onInput={inputHandler}
+        />
+        <Input
+          element="input"
+          type="password"
+          id="password"
+          label="Password"
+          errorText="Please Enter a valid password (requrie at least 8 characters"
+          validators={[VALIDATOR_MINLENGTH(8)]}
+          onInput={inputHandler}
+        />
+        <Button inverse onClick={switchModeHandler} type="button">
+          {isLoging ? "Switch to SignUp" : "Switch to Login"}
+        </Button>
+        <Button type="submit" disabled={!formState.isValid}>
+          {isLoging ? "Login" : "SignUp"}
+        </Button>
+      </form>
+    </Card>
+  );
+}
+
+export default Auth;
