492315fecf84236ab07a749c50f313876e06a9a4
Benzter <Dhanushkanuwanpremasiri28@gmail.com>
2023-04-03 17:56:43 +0530

    Add custom http-hook

---
+++ FRONTEND/src/shared/hooks/http-hook.js
@@ -0,0 +1,50 @@
+import { useCallback, useEffect, useRef, useState } from "react";
+
+function useHttpRequest() {
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState();
+
+  const activeHttpRequests = useRef([]);
+
+  const sendRequest = useCallback(
+    async (url, method = "GET", headers = {}, body = null) => {
+      setIsLoading(true);
+      const httpAbortCtrl = new AbortController();
+      activeHttpRequests.current.push(httpAbortCtrl);
+
+      try {
+        const response = await fetch(url, {
+          method,
+          headers,
+          body,
+          signal: httpAbortCtrl.signal,
+        });
+        const responseData = await response.json();
+        if (!response.ok) {
+          throw new Error(responseData.message);
+        }
+        setIsLoading(false);
+        return responseData;
+      } catch (err) {
+        setError(err.message);
+        setIsLoading(false);
+        throw err;
+      }
+    },
+    []
+  );
+
+  function clearError() {
+    setError(null);
+  }
+
+  useEffect(() => {
+    return () => {
+      activeHttpRequests.current.forEach((abortCtrl) => abortCtrl.abort());
+    };
+  }, []);
+
+  return { isLoading, error, sendRequest, clearError };
+}
+
+export default useHttpRequest;
