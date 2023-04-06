961fed1e18409acaee8c69f4f3d026e97ad93a78
Benzter <Dhanushkanuwanpremasiri28@gmail.com>
2023-04-06 10:50:09 +0530

    Install multer package to save data, And Add logic to create custom middleware using multer to save image data

--- BACKEND/routes/users-routes.js
+++ BACKEND/routes/users-routes.js
@@ -3,11 +3,13 @@ const { body } = require("express-validator");
 const router = express.Router();
 
 const userControllers = require("../controllers/users-controllers");
+const fileUpload = require("../middleware/file-upload");
 
 router.get("/", userControllers.getAllUsers);
 
 router.post(
   "/signup",
+  fileUpload.single("image"),
   [
     body("password").isLength({ min: 8 }),
     body("email").isEmail().normalizeEmail(),
