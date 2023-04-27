4588620be8892bfa498a037dce2c337c886c22ee
Benzter <Dhanushkanuwanpremasiri28@gmail.com>
2023-04-27 21:52:46 +0530

    Add check-auth.js to protect routes and get authorize users

---
+++ BACKEND/middleware/check-auth.js
@@ -0,0 +1,17 @@
+const jwt = require("jsonwebtoken");
+const HttpError = require("../models/http-error");
+
+const checkAuth = (req, res, next) => {
+  try {
+    const token = req.headers.authorization.split(" ")[1];
+    const decodeToken = jwt.verify(
+      token,
+      "This_is_a_secret_only_for_this_project_43ff54&^ds"
+    );
+    res.userData = { userId: decodeToken.userId };
+    next();
+  } catch (err) {
+    const error = new HttpError("Authentication failed", 401);
+    return next(error);
+  }
+};
