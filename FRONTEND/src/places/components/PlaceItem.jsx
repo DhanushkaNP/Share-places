5a8f09c1c270a78d108025db5ef7125fbf009c65
Benzter <Dhanushkanuwanpremasiri28@gmail.com>
2023-04-28 07:48:28 +0530

    Change code to send request with token when deleting place

--- FRONTEND/src/places/components/PlaceItem.jsx
+++ FRONTEND/src/places/components/PlaceItem.jsx
@@ -26,7 +26,8 @@ function PlaceItem(props) {
     try {
       await sendRequest(
         `http://localhost:5000/api/places/${props.id}`,
-        "DELETE"
+        "DELETE",
+        { Authorization: "Bearer " + auth.token }
       );
       props.onDelete(props.id);
     } catch (err) {}
