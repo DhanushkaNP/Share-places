b1734ed217e029c367b343f3402505d5f45ffc59
Benzter <Dhanushkanuwanpremasiri28@gmail.com>
2023-05-15 12:28:22 +0530

    Add Places.jsx component for code reusability purposes

---
+++ FRONTEND/src/shared/components/Container/Places.jsx
@@ -0,0 +1,43 @@
+import PlaceList from "../components/PlaceList";
+import useHttpRequest from "../../shared/hooks/http-hook";
+import React, { useEffect, useState } from "react";
+import ErrorModal from "../../shared/components/UIElements/ErrorModal";
+import LoadingSpinner from "../../shared/components/UIElements/LoadingSpinner";
+
+function Places(props) {
+  const [loadedPlaces, setLoadedPlaces] = useState();
+  const { isLoading, error, sendRequest, clearError } = useHttpRequest();
+
+  useEffect(() => {
+    async function getUserPlaces() {
+      const url = process.env.REACT_APP_BACKEND_URL + props.link;
+      try {
+        const responseData = await sendRequest(url);
+        setLoadedPlaces(responseData.places);
+      } catch (err) {}
+    }
+    getUserPlaces();
+  }, [props.link, sendRequest]);
+
+  function placeDeleteHandler(deletedPlaceId) {
+    setLoadedPlaces((prevPlaces) =>
+      prevPlaces.filter((place) => place.id !== deletedPlaceId)
+    );
+  }
+
+  return (
+    <React.Fragment>
+      <ErrorModal error={error} onClear={clearError} />
+      {isLoading && (
+        <div className="center">
+          <LoadingSpinner asOverlay />
+        </div>
+      )}
+      {!isLoading && loadedPlaces && (
+        <PlaceList items={loadedPlaces} onDeletePlace={placeDeleteHandler} />
+      )}
+    </React.Fragment>
+  );
+}
+
+export default Places;
