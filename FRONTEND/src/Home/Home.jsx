057b49b1b777c7fc8de7b5f7e708b93bca8fde05
Benzter <Dhanushkanuwanpremasiri28@gmail.com>
2023-05-24 21:48:02 +0530

    Added new Home.jsx component

---
+++ FRONTEND/src/Home/Home.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Home() {
+  return (
+    <div className="bg-gradient-to-b from-secondary from-5% to-white to-50% h-screen px-20 ">
+      <div className="grid lg:grid-cols-2 h-full p-12">
+        <div>1</div>
+        <div>2</div>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
