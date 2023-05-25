a5b6fb4ff2d9ae02e65f9a415b0de94150404a15
Benzter <Dhanushkanuwanpremasiri28@gmail.com>
2023-05-25 14:50:08 +0530

    Change PlaceList.jsx to render only six place card depending on the main prop

--- FRONTEND/src/places/components/PlaceList.jsx
+++ FRONTEND/src/places/components/PlaceList.jsx
@@ -1,9 +1,24 @@
+import { useEffect, useState } from "react";
 import Button from "../../shared/components/FormElements/Button";
 import Card from "../../shared/components/UIElements/Card";
 import PlaceItem from "./PlaceItem";
 import "./PlaceList.css";
 
 function PlaceList(props) {
+  const [sixPlaces, setSixPlaces] = useState([]);
+  console.log(props.items.length);
+  useEffect(() => {
+    if (props.items.length > 6) {
+      const result = props.items.slice(0, 6);
+      setSixPlaces(result);
+    } else {
+      console.log(props.items);
+      setSixPlaces(props.items);
+    }
+  }, [props.items]);
+
+  console.log(sixPlaces);
+
   if (props.items.length === 0) {
     return (
       <div className="place-list center">
@@ -14,24 +29,43 @@ function PlaceList(props) {
       </div>
     );
   }
-
-  return (
-    <ul className="place-list">
-      {props.items.map((place) => (
-        <PlaceItem
-          key={place.id}
-          id={place.id}
-          image={place.image}
-          title={place.title}
-          description={place.description}
-          address={place.address}
-          creatorId={place.creator}
-          coordinates={place.location}
-          onDelete={props.onDeletePlace}
-        />
-      ))}
-    </ul>
-  );
+  if (props.main) {
+    return (
+      <ul className="place-list">
+        {sixPlaces.map((place) => (
+          <PlaceItem
+            key={place.id}
+            id={place.id}
+            image={place.image}
+            title={place.title}
+            description={place.description}
+            address={place.address}
+            creatorId={place.creator}
+            coordinates={place.location}
+            onDelete={props.onDeletePlace}
+          />
+        ))}
+      </ul>
+    );
+  } else {
+    return (
+      <ul className="place-list">
+        {props.items.map((place) => (
+          <PlaceItem
+            key={place.id}
+            id={place.id}
+            image={place.image}
+            title={place.title}
+            description={place.description}
+            address={place.address}
+            creatorId={place.creator}
+            coordinates={place.location}
+            onDelete={props.onDeletePlace}
+          />
+        ))}
+      </ul>
+    );
+  }
 }
 
 export default PlaceList;
