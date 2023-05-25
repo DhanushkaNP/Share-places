d382ba4731855cd894912f2e38a8bfcbc20d2f73
Benzter <Dhanushkanuwanpremasiri28@gmail.com>
2023-05-25 18:00:44 +0530

    Separate Home.jsx code into HomeImage.jsx

--- FRONTEND/src/Home/pages/Home.jsx
+++ FRONTEND/src/Home/pages/Home.jsx
@@ -1,28 +1,22 @@
 import React from "react";
 import Places from "../../places/components/Places";
-import Button from "../../shared/components/FormElements/Button";
-import main from "../../assets/images/landing-main.png";
 import HomeContent from "../components/HomeContent";
+import HomeImage from "../components/HomeImage";
+import BackgroundSection from "../../shared/components/UIElements/BackgroundSection";
 
 function Home() {
   return (
     <React.Fragment>
-      <div className="bg-gradient-to-b from-secondary from-5% to-white to-50% h-max lg:px-16 ">
+      <BackgroundSection>
         <div className="lg:grid lg:grid-cols-2 h-max lg:p-12 pb-0">
           {/* first grid child */}
           <HomeContent />
 
           {/* second grid child */}
-
-          <div className="border-8 shadow-lg box-border overflow-hidden m-6 hidden lg:block">
-            <img
-              src={main}
-              alt="A person enjoying the view of mountain landscape"
-              className="w-full h-full object-cover"
-            />
-          </div>
+          <HomeImage />
         </div>
-      </div>
+      </BackgroundSection>
+
       <div className="h-min w-full bg-secondary/50">
         <Places main link={"/places/all"} />
       </div>
