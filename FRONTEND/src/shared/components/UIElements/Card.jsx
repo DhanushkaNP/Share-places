9206191fce2be52cb36661c3aaffd10b9ae8c425
Benzter <Dhanushkanuwanpremasiri28@gmail.com>
2023-05-26 12:11:26 +0530

    Adjust some styles

--- FRONTEND/src/shared/components/UIElements/Card.jsx
+++ FRONTEND/src/shared/components/UIElements/Card.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 
-import "./Card.css";
+// import "./Card.css";
 
 function Card(props) {
   return (
-    <div className={`card ${props.className}`} style={props.style}>
+    <div
+      className={` shadow-md rounded-md bg-white overflow-hidden ${props.className}`}
+      style={props.style}
+    >
       {props.children}
     </div>
   );
--- FRONTEND/src/shared/components/UIElements/CardHolder.jsx
+++ FRONTEND/src/shared/components/UIElements/CardHolder.jsx
@@ -2,7 +2,9 @@ import React from "react";
 
 export default function CardHolder(props) {
   return (
-    <div className={`grid md:grid-cols-2 lg:grid-cols-3  ${props.className}`}>
+    <div
+      className={`grid md:grid-cols-2 lg:grid-cols-3 gap-5 pt-10 pb-12 h-min ${props.className}`}
+    >
       {props.children}
     </div>
   );
