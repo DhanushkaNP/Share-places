be1ebc819db8e13db0c992e1826e36e533e34d04
Benzter <Dhanushkanuwanpremasiri28@gmail.com>
2023-05-26 09:24:53 +0530

    Change styles in BackgroundSection.jsx

--- FRONTEND/src/shared/components/UIElements/BackgroundSection.jsx
+++ FRONTEND/src/shared/components/UIElements/BackgroundSection.jsx
@@ -2,7 +2,9 @@ import React from "react";
 
 export default function BackgroundSection(props) {
   return (
-    <div className="bg-gradient-to-b from-secondary from-5% to-white to-50% h-max lg:px-16 ">
+    <div
+      className={`bg-gradient-to-b from-secondary from-5% to-white to-50% h-max px-4 lg:px-16 ${props.className}`}
+    >
       {props.children}
     </div>
   );
