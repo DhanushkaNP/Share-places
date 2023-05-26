f8845e278337beddd0e8bec948eeb50206e09327
Benzter <Dhanushkanuwanpremasiri28@gmail.com>
2023-05-26 12:12:23 +0530

    Import CardTitle.jsx to PlaceList.jsx and used it

--- FRONTEND/src/places/components/PlaceList.jsx
+++ FRONTEND/src/places/components/PlaceList.jsx
@@ -3,6 +3,9 @@ import Button from "../../shared/components/FormElements/Button";
 import Card from "../../shared/components/UIElements/Card";
 import PlaceItem from "./PlaceItem";
 import BackgroundSection from "../../shared/components/UIElements/BackgroundSection";
+import CardHolder from "../../shared/components/UIElements/CardHolder";
+import CardTitle from "./CardTitle";
+// import "./PlaceList.css";
 
 function PlaceList(props) {
   const [sixPlaces, setSixPlaces] = useState([]);
@@ -21,7 +24,7 @@ function PlaceList(props) {
 
   if (props.items.length === 0) {
     return (
-      <div className="place-list center">
+      <div className="">
         <Card>
           <h1>Nothing found!!!. Maybe create one?</h1>
           <Button to="/places/new">Share places</Button>
@@ -32,43 +35,49 @@ function PlaceList(props) {
   if (props.main) {
     return (
       <BackgroundSection className="bg-none mt-8 lg:mt-0">
-        <div className="pt-10 lg:pt-24">
-          <h3 className="text-4xl font-bold">Explore Places...</h3>
-          <p className=" font-light">
-            Places that other users shared. enjoy.....
-          </p>
-        </div>
-        {sixPlaces.map((place) => (
-          <PlaceItem
-            key={place.id}
-            id={place.id}
-            image={place.image}
-            title={place.title}
-            description={place.description}
-            address={place.address}
-            creatorId={place.creator}
-            coordinates={place.location}
-            onDelete={props.onDeletePlace}
-          />
-        ))}
+        <CardTitle
+          title="Explore Places..."
+          description="Places that other users shared. enjoy....."
+        />
+        <CardHolder>
+          {sixPlaces.map((place) => (
+            <PlaceItem
+              key={place.id}
+              id={place.id}
+              image={place.image}
+              title={place.title}
+              description={place.description}
+              address={place.address}
+              creatorId={place.creator}
+              coordinates={place.location}
+              onDelete={props.onDeletePlace}
+            />
+          ))}
+        </CardHolder>
       </BackgroundSection>
     );
   } else {
     return (
       <BackgroundSection>
-        {props.items.map((place) => (
-          <PlaceItem
-            key={place.id}
-            id={place.id}
-            image={place.image}
-            title={place.title}
-            description={place.description}
-            address={place.address}
-            creatorId={place.creator}
-            coordinates={place.location}
-            onDelete={props.onDeletePlace}
-          />
-        ))}
+        <CardTitle
+          title="All Places..."
+          description="Places that other users shared. "
+        />
+        <CardHolder>
+          {props.items.map((place) => (
+            <PlaceItem
+              key={place.id}
+              id={place.id}
+              image={place.image}
+              title={place.title}
+              description={place.description}
+              address={place.address}
+              creatorId={place.creator}
+              coordinates={place.location}
+              onDelete={props.onDeletePlace}
+            />
+          ))}
+        </CardHolder>
       </BackgroundSection>
     );
   }
