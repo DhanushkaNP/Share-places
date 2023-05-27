d0a294907b9d4bae513ec1deb26fa258d369570d
Benzter <Dhanushkanuwanpremasiri28@gmail.com>
2023-05-27 18:39:55 +0530

    Change Card.jsx to work with dynamic styles

--- FRONTEND/src/shared/components/UIElements/Card.jsx
+++ FRONTEND/src/shared/components/UIElements/Card.jsx
@@ -1,13 +1,15 @@
 import React from "react";
+import { twMerge } from "tailwind-merge";
 
 // import "./Card.css";
 
 function Card(props) {
+  const style = twMerge(
+    "shadow-md rounded-md bg-white overflow-hidden",
+    props.className
+  );
   return (
-    <div
-      className={` shadow-md rounded-md bg-white overflow-hidden ${props.className}`}
-      style={props.style}
-    >
+    <div className={style} style={props.style}>
       {props.children}
     </div>
   );
