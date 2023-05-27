f8c9d93e9ae31770f24fb1d534aa19bfe6afb6b8
Benzter <Dhanushkanuwanpremasiri28@gmail.com>
2023-05-27 18:40:23 +0530

    Added new tailwind css styles

--- FRONTEND/src/user/components/UserItem.jsx
+++ FRONTEND/src/user/components/UserItem.jsx
@@ -1,28 +1,28 @@
 import Avatar from "../../shared/components/UIElements/Avatar";
 import Card from "../../shared/components/UIElements/Card";
-import "./UserItem.css";
+// import "./UserItem.css";
 import { Link } from "react-router-dom";
 
 function UserItem(props) {
   return (
-    <li className="user-item">
-      <Card className="user-item-content">
-        <Link to={`${props.id}/places `}>
-          <div className="user-item-image">
+    <Card className=" bg-transparent rounded-xl shadow-none hover:shadow-lg bg-[#D2F1FB]">
+      <Link to={`/${props.id}/places`}>
+        <div className="flex flex-col w-full pt-2 lg:pt-0 lg:flex-row gap-1 lg:gap-5 lg:m-1 lg:w-max">
+          <div className="h-24 w-24 flex flex-row mx-auto">
             <Avatar
               image={process.env.REACT_APP_ASSEST_URL + props.image}
               alt={props.name}
             />
           </div>
-          <div className="user-item-info">
-            <h2>{props.name}</h2>
-            <h3>
+          <div className="pt-2 text-center lg:text-left pb-2 lg:pb-0">
+            <h2 className=" text-xl font-semibold">{props.name}</h2>
+            <h3 className=" text-gray-600">
               {props.placeCount} {props.placeCount === 1 ? "place" : "places"}
             </h3>
           </div>
-        </Link>
-      </Card>
-    </li>
+        </div>
+      </Link>
+    </Card>
   );
 }
 
--- FRONTEND/src/user/components/UsersList.jsx
+++ FRONTEND/src/user/components/UsersList.jsx
@@ -1,3 +1,4 @@
+import BackgroundSection from "../../shared/components/UIElements/BackgroundSection";
 import Card from "../../shared/components/UIElements/Card";
 import UserItem from "./UserItem";
 import "./UsersList.css";
@@ -14,19 +15,21 @@ function UsersList(props) {
   }
 
   return (
-    <ul className="users-list">
-      {props.items.map((user) => {
-        return (
-          <UserItem
-            key={user.id}
-            id={user.id}
-            image={user.image}
-            name={user.name}
-            placeCount={user.places.length}
-          />
-        );
-      })}
-    </ul>
+    <BackgroundSection className=" h-screen">
+      <div className=" grid lg:grid-cols-2 gap-8 w-1/2 mx-auto pt-10">
+        {props.items.map((user) => {
+          return (
+            <UserItem
+              key={user.id}
+              id={user.id}
+              image={user.image}
+              name={user.name}
+              placeCount={user.places.length}
+            />
+          );
+        })}
+      </div>
+    </BackgroundSection>
   );
 }
 
