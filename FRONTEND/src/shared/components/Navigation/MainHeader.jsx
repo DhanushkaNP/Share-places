3885a1adf70889bdec0ce06da6816f99b04e2a24
Benzter <Dhanushkanuwanpremasiri28@gmail.com>
2023-05-31 17:04:34 +0530

    In MainHeader.jsx added code to hide navigation bar depend on the navContext values

--- FRONTEND/src/shared/components/Navigation/MainHeader.jsx
+++ FRONTEND/src/shared/components/Navigation/MainHeader.jsx
@@ -1,8 +1,15 @@
+import { useContext } from "react";
 import "./MainHeader.css";
+import { NavContext } from "../../context/nav-context";
 
 function MainHeader(props) {
+  const nav = useContext(NavContext);
   return (
-    <header className="bg-secondary h-20 z-10 flex flex-initial">
+    <header
+      className={`bg-secondary h-20 z-10 ${
+        nav.isNavigationOn ? "flex" : "hidden"
+      } flex-initial`}
+    >
       {props.children}
     </header>
   );
