4fc652ec791463d9f119e99d866765004ce60b8a
Benzter <Dhanushkanuwanpremasiri28@gmail.com>
2023-06-01 10:51:00 +0530

    Wrap nav.setNav(true) within useEffect to avoid unncessary renders

--- FRONTEND/src/shared/components/Navigation/NavigationTrigger.jsx
+++ FRONTEND/src/shared/components/Navigation/NavigationTrigger.jsx
@@ -1,9 +1,11 @@
-import { useContext } from "react";
+import { useContext, useEffect } from "react";
 import { NavContext } from "../../context/nav-context";
 
 export default function NavigationTrigger() {
   const nav = useContext(NavContext);
-  nav.setNav(true);
+  useEffect(() => {
+    nav.setNav(true);
+  }, [nav]);
 
   return null;
 }
